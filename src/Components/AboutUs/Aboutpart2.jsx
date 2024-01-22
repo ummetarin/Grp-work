09cec2678bcedd47683382db1fe4d84352b54f04
ummetarin <ummehomairatarin@gmail.com>
2024-01-22 21:49:56 +0600

    refine about us section

--- src/Components/AboutUs/Aboutpart2.jsx
+++ src/Components/AboutUs/Aboutpart2.jsx
@@ -8,16 +8,16 @@ const Aboutpart2 = () => {
             <img className="w-[450px] h-[300px] rounded-lg " src="https://i.ibb.co/cQGpqFc/phc-infographic-1.jpg" alt="" />
         </div>
         <div className="md:w-[470px] md:ml-0  ">
-            <h1 className="md:text-3xl py-2 text-blue-700 font-bold  ">Why We Uniqe From Others??</h1>
+            <h1 className="md:text-3xl py-2 text-[#0360D9] font-bold  ">Why We Uniqe <span className="text-[#021526]">From Others??</span></h1>
         <ul className="list-decimal ml-5">
             
-        <p className=" text-zinc-950 py-1 font-bold ">1.Cultural and Societal Values</p>
-            <p className=" text-zinc-950 py-1 font-bold">2.Healthcare Policies</p>
-            <p className=" text-zinc-950 py-1 font-bold ">3.Public and Private Sector Involvement</p>
-            <p className=" text-zinc-950 py-1 font-bold">4.Healthcare Financing</p>
-            <p className=" text-zinc-950 py-1 font-bold">5.Healthcare Infrastructure</p>
-            <p className=" text-zinc-950 py-1 font-bold">6.Emphasis on Specialized Care</p>
-            <p className=" text-zinc-950 py-1 font-bold">7.Innovation and Research</p>  
+        <p className=" text-[#2E2E2E] py-1 font-medium ">1.Cultural and Societal Values</p>
+            <p className=" text-[#2E2E2E] py-1 font-medium">2.Healthcare Policies</p>
+            <p className=" text-[#2E2E2E] py-1 font-medium ">3.Public and Private Sector Involvement</p>
+            <p className=" text-[#2E2E2E] py-1 font-medium">4.Healthcare Financing</p>
+            <p className=" text-[#2E2E2E] py-1 font-medium">5.Healthcare Infrastructure</p>
+            <p className=" text-[#2E2E2E] py-1 font-medium">6.Emphasis on Specialized Care</p>
+            <p className=" text-[#2E2E2E] py-1 font-medium">7.Innovation and Research</p>  
             </ul>       
         </div>
        
